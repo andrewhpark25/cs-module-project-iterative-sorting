b1082aa09bc1e192c455978eebf60c3474d78caa
andrewhpark25 <andrewhpark25@gmail.com>
2020-06-15 13:14:20 -0700

    Andrew-Park

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,13 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
@@ -18,7 +21,14 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    arrayLength = len(arr)
+    # Traverse through all array elements 
+    for i in range(arrayLength-1):
+        for j in range(0, arrayLength-i-1):
+            # traverse the array from 0 to n-i-1. Swap if the element found is greater than next 
+           if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+                    
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -8,7 +10,24 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    # Set boundaries for low and high marks to search
 
+    low = 0
+    high = len(arr)
+
+    # While low and high don't overlap
+
+    while low < high:
+        # Check midpoint
+        mid = (low + high) // 2
+    # Return true if equal
+        if arr[mid] == target:
+            return mid
+    # Else, if target is smaller, set high to midpoint value
+        elif target < arr[mid]:
+            high = mid
+    # Else, if target is bigger, set low to modpoint value
+        else:
+            low = mid + 1
 
     return -1  # not found
